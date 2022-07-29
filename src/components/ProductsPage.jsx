a79caba0cfca372080490ff87e36459a31c5f542
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-07-29 17:48:43 +0200

    iteration 4 done

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -5,11 +5,20 @@ import ProductTable from './ProductTable'
 
 export default function ProductsPage() {
     const [products, setProducts] = useState(jsonData)
+
+    const handleSearch = (searchValue) => {
+        if (searchValue === '') {
+            setProducts(jsonData)
+        } else {
+            const filtered = products.filter(el => el.name.toLowerCase().includes((searchValue).toLowerCase()))
+            setProducts(filtered)
+        }
+    }
     return (
         <div>
             <h1>IronStore</h1>
             <div>
-                <SearchBar />
+                <SearchBar onSearch={handleSearch} />
             </div>
             <div>
                 <ProductTable products={products} />
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function () {
+export default function (props) {
+    const { onSearch } = props;
     return (
         <div>
             <label htmlFor="">Search</label>
-            <input type="text" placeholder='Search' />
+            <input type="text" placeholder='Search' onChange={(e) => onSearch(e.target.value)} />
             <input type="checkbox" id='checkbox' />
             <label htmlFor="checkbox">Only show products in stock</label>
         </div>
